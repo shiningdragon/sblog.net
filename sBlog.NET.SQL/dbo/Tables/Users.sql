d366420350899ae67b4b37b288596756202246cf
Hamada <speakwithjoe@hotmail.co.uk>
2016-10-30 08:16:14 +0100

    Test code and db schema change

--- sBlog.NET.SQL/dbo/Tables/Users.sql
+++ sBlog.NET.SQL/dbo/Tables/Users.sql
@@ -10,6 +10,7 @@
     [UserCode]         VARCHAR (128) NULL,
     [UserSite]         VARCHAR (128) NULL,
     [LastLoginDate]    DATETIME      NULL,
+    [FavouriteColour] VARCHAR(50) NULL, 
     CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([UserID] ASC)
 );
 
